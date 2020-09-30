632d7e497f9ad534a2798eb7e009c22ad1b3bfed
Kostya808 <kostya.shefer.999@gmail.com>
2020-09-30 22:33:42 +0700

    Lab2: Changing the appearance of the specs.

--- labs/lab2/spec/calculate_spec.rb
+++ labs/lab2/spec/calculate_spec.rb
@@ -1,19 +1,26 @@
 require './lib/calculate'
 
-describe Calculate do
-  it 'Поиск максимума' do
-    expect(Calculate.search_max(['A,1', 'B,2', 'C,3', 'D,-1', 'E,5'])).to eq 'E,5'
-  end
+RSpec.describe Calculate do
+  describe 'Computation by file' do
+    let(:search_max) { Calculate.search_max(['A,1', 'B,2', 'C,3', 'D,-1', 'E,5']) }
+    let(:search_min) { Calculate.search_min(['A,1', 'B,2', 'C,3', 'D,-1', 'E,5']) }
+    let(:calc_mean) { Calculate.calculate_average(['A,1', 'B,2', 'C,3', 'D,-1', 'E,5']) }
+    let(:calc_csv) { Calculate.calculate_correct_sample_variance(['A,1', 'B,2', 'C,3', 'D,-1', 'E,5']) }
 
-  it 'Поиск минимума' do
-    expect(Calculate.search_min(['A,1', 'B,2', 'C,3', 'D,-1', 'E,5'])).to eq 'D,-1'
-  end
+    context 'Search maximum' do
+      it { expect(search_max).to eq 'E,5' }
+    end
 
-  it 'Поиск среднего' do
-    expect(Calculate.calculate_average(['A,1', 'B,2', 'C,3', 'D,-1', 'E,5'])).to eq '2'.to_f
-  end
+    context 'Search minimum' do
+      it { expect(search_min).to eq 'D,-1' }
+    end
 
-  it 'Вычисление исправленной выборочной дисперсии' do
-    expect(Calculate.calculate_correct_sample_variance(['A,1', 'B,2', 'C,3', 'D,-1', 'E,5'])).to eq '5'.to_f
+    context 'Search mean' do
+      it { expect(calc_mean).to eq 2.0 }
+    end
+
+    context 'Search maximum' do
+      it { expect(calc_csv).to eq 5.0 }
+    end
   end
 end
